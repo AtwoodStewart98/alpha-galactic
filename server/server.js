c5573a547a88d3393784163a03f014605fab49e5
Stewart Atwood <stewartatwood@Stewarts-MBP.fios-router.home>
2018-01-28 12:48:04 -0600

    setting up node server

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cors = require("cors");
+const massive = require("massive");
+const session = require("express-session");
+
+const config = require("./config.js");
+
+const port = 4200;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: config.secret,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+app.listen(port, () => {
+  console.log(`It's Over ${port}!`);
+});

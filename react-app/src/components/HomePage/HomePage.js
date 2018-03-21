92f70c5e7eb2396eae6af002e1260e29f8136db8
Stewart Atwood <stewartatwood@Stewarts-MacBook-Pro.local>
2018-03-21 14:51:38 -0500

    adding more standin photos

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -4,6 +4,10 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
 import { getUser } from "../../ducks/reducer.js";
+import cityscape from "../../assets/cityscape.png";
+import spacestation from "../../assets/spacestation.png";
+import illithidalien from "../../assets/illithidalien.png";
+import mercenaries from "../../assets/mercenaries.png";
 
 import "../../react-scss/react-css/HomePage.css";
 
@@ -35,20 +39,64 @@ class HomePage extends Component {
             </p>
           </div>
         </div>
-        <div className="linx-flex">
-          <div>
-            <Link to="/characterMaker">Create your own Character</Link>
-          </div>
-          <div>
-            <Link to="/encounterGenerator">Spawn Random Encounters</Link>
+        <div className="section-two">
+          <h2>FIND YOUR PLACE IN THE GALAXY</h2>
+          <img className="cityscape" src={cityscape} alt="cityscape" />
+          <div className="cityscape-flex">
+            <div>
+              <h2>BECOME A GAME MASTER</h2>
+              <p>
+                Sign up for a full account to become a game master and design
+                your own campaigns!
+              </p>
+            </div>
+            <div className="cityscape-descriptor">
+              <p>
+                As a player, you will be able to create and save up to five
+                characters to your account, with traits chosen from a multitude
+                of introductory questions in the character creator, including
+                alignment, race, background experience, and so forth. Whether
+                you are a lone mercenary picking up odd jobs for the exploratory
+                experience, or a stalwart defender of the Galactic Alliance, or
+                such is up to you!
+              </p>
+              <p>
+                As a game master you will be able to create your own missions
+                and story arcs in the campaign manager, with full access to lore
+                and various enemy factions represented throughout the galaxy.
+                Organize a group of friends to work together as a team to
+                explore strange new worlds, meet aliens from all sorts of races
+                and backgrounds, and clash with infamous villains who scheme
+                throughout the galaxy!
+              </p>
+            </div>
           </div>
         </div>
-        <div className="linx-flex">
-          <div>
-            <Link to="/weaponGenerator">Arm your Character</Link>
-          </div>
-          <div>
-            <Link to="/auth">Login / Sign Up</Link>
+        <div className="linx">
+          <h2>GAME FEATURES</h2>
+          <p>
+            To learn more about how to play Alpha Galactic, follow each of the
+            below links!
+          </p>
+          <div className="linx-flex">
+            <div className="space-station">
+              <Link to="/characterMaker">
+                <img src={spacestation} alt="space station" />
+                <p>Create your own Character</p>
+              </Link>
+            </div>
+            <div className="illithid-alien">
+              <Link to="/encounterGenerator">
+                <img src={illithidalien} alt="alien" />
+                <p>Spawn Random Encounters</p>
+              </Link>
+            </div>
+            <div className="mercenaries">
+              <Link to="/weaponGenerator">
+                <img src={mercenaries} alt="mercenaries" />
+                <p>Arm your Character</p>
+              </Link>
+            </div>
           </div>
         </div>
       </div>

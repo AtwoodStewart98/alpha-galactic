1cf1ec7ba7e576a8affdf42cba90281076a01caa
Stewart Atwood <stewartatwood@Stewarts-MacBook-Pro.local>
2018-03-22 18:50:32 -0500

    fixed positioning issue on race questionnaire

--- react-app/src/components/CharacterMaker/QFour.js
+++ react-app/src/components/CharacterMaker/QFour.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import { updateQuestionNumber, updateRace } from "../../ducks/reducer.js";
+import Xiphave from "../../assets/xiphaverace.png";
+import Arktonian from "../../assets/arktonianrace.png";
+import Thrax from "../../assets/thraxrace.png";
+import Holkovrhys from "../../assets/holkovrhysrace.png";
+import Porturuxean from "../../assets/porturuxeanrace.png";
+import Draconis from "../../assets/draconisrace.png";
+import Gob from "../../assets/gobrace.png";
+import Tomlin from "../../assets/tomlinrace.png";
+import Arthrotera from "../../assets/arthroterarace.png";
 
 class QFour extends Component {
   render() {
@@ -9,11 +18,10 @@ class QFour extends Component {
 
     return (
       <div
-        className="q-four"
         style={{ display: this.props.questionNumber === 4 ? "block" : "none" }}
       >
         <p>What is your character&#39;s race?</p>
-        <form className="question-block">
+        <form className="question-block race-questions">
           <p>
             <input
               className="radio-selector"
@@ -21,13 +29,16 @@ class QFour extends Component {
               onClick={e => updateRace("Porturuxean")}
               name="q4"
             />
+            <img src={Porturuxean} alt="race1" width="170px" />
             <label>
-              <strong>Porturuxean</strong> - Humanoid race with distinctive
-              crimson skin tone and feathered wings extending from the shoulder
-              blades which been rendered vestigial after generations of disuse;
-              only strong enough for gliding. Slightly protruding jaws with
-              sharp canine teeth for carnivorous consumption. Belonging to one
-              of the most prominent races in the Galactic Alliance.
+              <strong>PORTURUXEAN</strong>
+              <br />
+              Humanoid race with distinctive crimson skin tone and feathered
+              wings extending from the shoulder blades which been rendered
+              vestigial after generations of disuse; only strong enough for
+              gliding. Slightly protruding jaws with sharp canine teeth for
+              carnivorous consumption. Belonging to one of the most prominent
+              races in the Galactic Alliance.
             </label>
           </p>
           <p>
@@ -37,12 +48,15 @@ class QFour extends Component {
               onClick={e => updateRace("Arthrotera")}
               name="q4"
             />
+            <img src={Arthrotera} alt="race2" width="170px" />
             <label>
-              <strong>Arthrotera</strong> - Insectoid race reminiscent of a stag
-              beetle with a dark brown exoskeleton that has a green sheen in
-              direct lighting. Six compound eyes allow them to detect even the
-              tiniest movements, but they are particularly vulnerable to
-              disorientating visuals. Another member of the Galactic Alliance.
+              <strong>ARTHROTERA</strong>
+              <br />
+              Insectoid race reminiscent of a stag beetle with a dark brown
+              exoskeleton that has a green sheen in direct lighting. Six
+              compound eyes allow them to detect even the tiniest movements, but
+              they are particularly vulnerable to disorientating visuals.
+              Another member of the Galactic Alliance.
             </label>
           </p>
           <p>
@@ -52,13 +66,15 @@ class QFour extends Component {
               onClick={e => updateRace("Draconis")}
               name="q4"
             />
+            <img src={Draconis} alt="race3" width="170px" />
             <label>
-              <strong>Draconis</strong> - Reptilian lizardlike race with varying
-              colored scales ranging from jet black to ochre and sandy brown.
-              There exist two subspecies; one that possesses leathery wings with
-              enough strength to allow low levels of flight, and one with a
-              combustion organ in their chest that lets them exhale fire at a
-              substantial physical drain.
+              <strong>DRACONIS</strong>
+              <br />
+              Reptilian lizardlike race with varying colored scales ranging from
+              jet black to ochre and sandy brown. There exist two subspecies;
+              one that possesses leathery wings with enough strength to allow
+              low levels of flight, and one with a combustion organ in their
+              chest that lets them exhale fire at a substantial physical drain.
             </label>
           </p>
           <p>
@@ -68,12 +84,15 @@ class QFour extends Component {
               onClick={e => updateRace("Gob")}
               name="q4"
             />
+            <img src={Gob} alt="race4" width="170px" />
             <label>
-              <strong>Gob</strong> - Short-statured humanoid race with thin lime
-              green skin, and possess efficient auditory, olfactory, and visual
-              senses. Former enemy race of the Galactic Alliance, but have made
-              a complete turnaround and now make up a vast majority of the
-              Galactic Department of Security.
+              <strong>GOB</strong>
+              <br />
+              Short-statured humanoid race with thin lime green skin, and
+              possess efficient auditory, olfactory, and visual senses. Former
+              enemy race of the Galactic Alliance, but have made a complete
+              turnaround and now make up a vast majority of the Galactic
+              Department of Security.
             </label>
           </p>
           <p>
@@ -83,12 +102,15 @@ class QFour extends Component {
               onClick={e => updateRace("Holkovrhys")}
               name="q4"
             />
+            <img src={Holkovrhys} alt="race5" width="170px" />
             <label>
-              <strong>Holkovrhys</strong> - Towering reptilian race with small
-              indigo scales and bony horns curving back from the forehead; males
-              have two large horns and females have a crown of smaller spikes.
-              Prideful of their culture, bitterly racist towards Porturuxeans
-              and vehement enemies of the Galactic Alliance.
+              <strong>HOLKOVRHYS</strong>
+              <br />
+              Towering reptilian race with small indigo scales and bony horns
+              curving back from the forehead; males have two large horns and
+              females have a crown of smaller spikes. Prideful of their culture,
+              bitterly racist towards Porturuxeans and vehement enemies of the
+              Galactic Alliance.
             </label>
           </p>
           <p>
@@ -98,12 +120,14 @@ class QFour extends Component {
               onClick={e => updateRace("Xiphave")}
               name="q4"
             />
+            <img src={Xiphave} alt="race6" width="170px" />
             <label>
-              <strong>Xiphave</strong> - Leathery-skinned, hairless race with
-              bony features and a distinct flat, &#34;winged&#34; skull
-              structure. External fangs protrude around the mouth, and their
-              slit irises allow nightvision. Only race that totally comprises
-              the Hyjakker space pirate empire.
+              <strong>XIPHAVE</strong>
+              <br />
+              Leathery-skinned, hairless race with bony features and a distinct
+              flat, &#34;winged&#34; skull structure. External fangs protrude
+              around the mouth, and their slit irises allow nightvision. Only
+              race that totally comprises the Hyjakker space pirate empire.
             </label>
           </p>
           <p>
@@ -113,13 +137,16 @@ class QFour extends Component {
               onClick={e => updateRace("Thrax")}
               name="q4"
             />
+            <img src={Thrax} alt="race7" width="170px" />
             <label>
-              <strong>Thrax</strong> - Humanoid race with tangerine-colored skin
-              related to the Gob, but considered separate due to their obsession
-              with genetic enhancements, deviating themselves from their
-              biological cousins. Strongly atheistic and anti-religion, and
-              almost always seen with open cybernetics. Raiding race with
-              complete territory of the center of the galaxy.
+              <strong>THRAX</strong>
+              <br />
+              Humanoid race with tangerine-colored skin related to the Gob, but
+              considered separate due to their obsession with genetic
+              enhancements, deviating themselves from their biological cousins.
+              Strongly atheistic and anti-religion, and almost always seen with
+              open cybernetics. Raiding race with complete territory of the
+              center of the galaxy.
             </label>
           </p>
           <p>
@@ -129,13 +156,15 @@ class QFour extends Component {
               onClick={e => updateRace("Arktonian")}
               name="q4"
             />
+            <img src={Arktonian} alt="race8" width="170px" />
             <label>
-              <strong>Arktonian</strong> - Insectoid race reminiscent of a cross
-              between a centaur and a scorpion, with four legs supporting a
-              carapaced torso, and a back end connected to a tail with a
-              venomous stinger, which takes a week or so to regrow after use.
-              Does not possess auditory senses, and communicates using body
-              language and pheromones.
+              <strong>ARKTONIAN</strong>
+              <br />
+              Insectoid race reminiscent of a cross between a centaur and a
+              scorpion, with four legs supporting a carapaced torso, and a back
+              end connected to a tail with a venomous stinger, which takes a
+              week or so to regrow after use. Does not possess auditory senses,
+              and communicates using body language and pheromones.
             </label>
           </p>
           <p>
@@ -145,12 +174,15 @@ class QFour extends Component {
               onClick={e => updateRace("Tomlin")}
               name="q4"
             />
+            <img src={Tomlin} alt="race9" width="170px" />
             <label>
-              <strong>Tomlin</strong> - Avian race feathered in various shades
-              of green and fully developed, strong wings, but no true arms.
-              Among their four legs, the front pair have semi-opposable claws
-              that allow handling of objects, with minor difficulty. Brilliantly
-              empathetic and extremely social.
+              <strong>TOMLIN</strong>
+              <br />
+              Avian race feathered in various shades of green and fully
+              developed, strong wings, but no true arms. Among their four legs,
+              the front pair have semi-opposable claws that allow handling of
+              objects, with minor difficulty. Brilliantly empathetic and
+              extremely social.
             </label>
           </p>
         </form>

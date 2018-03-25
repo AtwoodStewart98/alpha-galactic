17bc0636496bce660325ee5653b426a61d7a63ff
Stewart Atwood <stewartatwood@Stewarts-MacBook-Pro.local>
2018-03-25 17:18:18 -0500

    regular commit

--- react-app/src/components/CharacterMaker/QResults.js
+++ react-app/src/components/CharacterMaker/QResults.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import Xiphave from "../../assets/xiphaverace.png";
+import Arktonian from "../../assets/arktonianrace.png";
+import Thrax from "../../assets/thraxrace.png";
+import Holkovrhys from "../../assets/holkovrhysrace.png";
+import Porturuxean from "../../assets/porturuxeanrace.png";
+import Draconis from "../../assets/draconisrace.png";
+import Gob from "../../assets/gobrace.png";
+import Tomlin from "../../assets/tomlinrace.png";
+import Arthrotera from "../../assets/arthroterarace.png";
+
 import { updateQuestionNumber, resetQuestions } from "../../ducks/reducer.js";
 
 class QResults extends Component {
@@ -11,12 +21,15 @@ class QResults extends Component {
       <div
         style={{ display: this.props.questionNumber === 10 ? "block" : "none" }}
       >
-        <h1>Results:</h1>
-        <p>Name: {this.props.charName}</p>
-        <p>Alignment: {this.props.trueAlignment}</p>
-        <p>Race: {this.props.race}</p>
-        <p>Experience: {this.props.training}</p>
-        <p>Faction: {this.props.faction}</p>
+        <div>
+          {this.props.race !== "" ? (
+            <img src={this.props.race} alt="race" />
+          ) : null}
+          <p>Name: {this.props.charName}</p>
+          <p>Alignment: {this.props.trueAlignment}</p>
+          <p>Experience: {this.props.training}</p>
+          <p>Faction: {this.props.faction}</p>
+        </div>
         <br />
         <button
           onClick={() => {
